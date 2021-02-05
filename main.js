99ea088952077547841c7da26d359770363dcfcc
John Martin De Guia <63047587+jmdeguia09@users.noreply.github.com>
2021-02-05 16:19:47 +0800

    Added New Data

--- main.js
+++ main.js
@@ -8,20 +8,21 @@ const humidity = document.querySelector('.humidity');
 // Fetch API Function
 
 submit.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=b23efc626c5c1777ad76c81c8fc96bab')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=b23efc626c5c1777ad76c81c8fc96bab&units=metric')
     .then(response => response.json())
     .then(data => {
-        const nameValue = data['name'];
+        const nameValue = data['name'] + ', ' + data['sys']['country'];
         const tempValue = data['main']['temp'];
         const descValue = data['weather'][0]['description'];
         const humidValue = data['main']['humidity'];
         
         name.innerHTML = nameValue;
-        temperature.innerHTML = 'Temperature: ' + tempValue;
-        description.innerHTML = descValue;
+        temperature.innerHTML = 'Temperature: ' + tempValue + ' °C';
+        description.innerHTML = '<img src="http://openweathermap.org/img/wn/'+ data.weather[0].icon +'.png"> <br>' + descValue;
         humidity.innerHTML = 'Humidity: ' + humidValue;
 
+        // Clears the text box if a city is submitted
         input.value = "";
     })   // Error Message if City name is wrong
-    .catch(err => alert("Invalid City Name. Please Enter a Valid City Name"));
+    .catch(err => alert("City not available. Please enter another city."));
 })
